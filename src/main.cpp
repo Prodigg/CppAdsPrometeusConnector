635302aa8ceedfbd924e2091583553e27a1a60ae
prodigg <raphael@familie-sauer.ch>
2026-01-22 20:56:25 +0100

    added REST lib and added test REST endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,19 @@
 #include "AdsDevice.h"
 #include "AdsVariable.h"
 
+#include <pistache/endpoint.h>
+#include <pistache/router.h>
+
+using namespace Pistache;
+
+void test(const Rest::Request& request, Http::ResponseWriter response) {
+    response.send(Http::Code::Ok, "endpoint is ok\n");
+}
+
 int main() {
 
     static const AmsNetId remoteNetId{5, 109, 7, 180, 1, 1};
-    static constexpr char remoteIpV4[] = "192.168.1.129";
+    static constexpr char remoteIpV4[] = "plc-raphael";
     bhf::ads::SetLocalAddress({192,168,1,126,1,1});
 
     AdsDevice route{ remoteIpV4, remoteNetId, AMSPORT_R0_PLC_TC3 };
@@ -15,6 +24,19 @@ int main() {
     AdsVariable<bool> readVar{ route, "NodeRed.bIsLightOn" };
 
     std::cout << "NodeRed.bIsLightOn: " << (bool)readVar <<" \n";
+
+
+    Address addr(Ipv4::any(), Port(9080));
+
+    auto opts = Http::Endpoint::options().threads(1);
+    Http::Endpoint endpoint(addr);
+    Rest::Router router;
+
+    Rest::Routes::Get(router, "/test", Rest::Routes::bind(&test));
+
+    endpoint.init(opts);
+    endpoint.setHandler(router.handler());
+    endpoint.serve();
 
     return 0;
 }
