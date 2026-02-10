28f09086f8e224d94884d175599ed228cef55b15
prodigg <raphael@familie-sauer.ch>
2026-02-10 22:32:19 +0100

    further implementation of thread and constructor / destructor

---
+++ src/ADSProvidor.cpp
@@ -0,0 +1,167 @@
+//
+// Created by prodigg on 04.02.26.
+//
+#include "ADSProvidor.h"
+
+#include <mutex>
+
+AdsProvidor_t::AdsProvidor_t(ProcessDataBuffer_t& processDataBuffer, AmsNetId remoteAmsNetId, std::string remoteIPv4, AmsNetId localAmsNetId) :
+    _processDataBuffer(processDataBuffer) {
+    bhf::ads::SetLocalAddress(localAmsNetId);
+    _device.emplace(remoteIPv4, remoteAmsNetId, AMSPORT_R0_PLC_TC3);
+    _thread.emplace(threadLoop);
+}
+
+AdsProvidor_t::~AdsProvidor_t() {
+    _thread.reset();
+    _device.reset();
+}
+
+void AdsProvidor_t::addSymbol(const std::string& symbolName, symbolDataType_t symbolType, std::chrono::high_resolution_clock::duration scrapingTime) {
+    std::scoped_lock(_symbolNamesMutex);
+    _symbolNames.emplace_back(symbolName, symbolType, scrapingTime, std::chrono::high_resolution_clock::now());
+}
+
+void AdsProvidor_t::updateSymbolProcessDataBuffer(symbolDefinition_t& symbolDefinition, const std::string value) const {
+    _processDataBuffer.setSymbolValue(symbolDefinition.symbolName, value);
+    symbolDefinition.lastRead = std::chrono::high_resolution_clock::now();
+}
+
+void AdsProvidor_t::threadLoop() {
+    {
+        std::scoped_lock(_symbolNamesMutex);
+        forceReadSymbol();
+    }
+    const auto start = std::chrono::high_resolution_clock::now();
+    while (start + std::chrono::milliseconds(500) > std::chrono::high_resolution_clock::now())
+        ; // wait to allow for addSymbol to insert data into _symbolName
+}
+
+void AdsProvidor_t::forceReadSymbol() {
+    for (symbolDefinition_t& symbol: _symbolNames) {
+        if (symbol.lastRead + symbol.expirationDuration <= std::chrono::high_resolution_clock::now())
+            readSymbol(symbol);
+    }
+}
+
+void AdsProvidor_t::readSymbol(symbolDefinition_t& symbolDefinition) {
+    switch (symbolDefinition.symbolType) {
+        case symbolDataType_t::e_bool:
+            const AdsVariable<bool> readVarBool{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<bool>(readVarBool));
+            break;
+        case symbolDataType_t::e_char:
+            const AdsVariable<char> readVarChar{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<char>(readVarChar));
+            break;
+        case symbolDataType_t::e_double:
+            const AdsVariable<double> readVarDouble{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<double>(readVarDouble));
+            break;
+        case symbolDataType_t::e_float:
+            const AdsVariable<float> readVarFloat{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<float>(readVarFloat));
+            break;
+        case symbolDataType_t::e_int8_t:
+            const AdsVariable<int8_t> readVarInt8{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<int8_t>(readVarInt8));
+            break;
+        case symbolDataType_t::e_int16_t:
+            const AdsVariable<int16_t> readVarInt16{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<int16_t>(readVarInt16));
+            break;
+        case symbolDataType_t::e_int32_t:
+            const AdsVariable<int32_t> readVarInt32{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<int32_t>(readVarInt32));
+            break;
+        case symbolDataType_t::e_int64_t:
+            const AdsVariable<int64_t> readVarInt64{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<int64_t>(readVarInt64));
+            break;
+        case symbolDataType_t::e_string:
+            const AdsVariable<std::string> readVarString{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, readVarString);
+            break;
+        case symbolDataType_t::e_uint8_t:
+            const AdsVariable<uint8_t> readVarUint8{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<uint8_t>(readVarUint8));
+            break;
+        case symbolDataType_t::e_uint16_t:
+            const AdsVariable<uint16_t> readVarUint16{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<uint16_t>(readVarUint16));
+            break;
+        case symbolDataType_t::e_uint32_t:
+            const AdsVariable<uint32_t> readVarUint32{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<uint32_t>(readVarUint32));
+            break;
+        case symbolDataType_t::e_uint64_t:
+            const AdsVariable<uint64_t> readVarUint64{ _device.value(), symbolDefinition.symbolName };
+            updateSymbolProcessDataBuffer(symbolDefinition, static_cast<uint64_t>(readVarUint64));
+            break;
+        default:
+            throw std::runtime_error("unknown symbol type");
+    }
+}
+
+void AdsProvidor_t::subscribeSymbols() {
+    throw std::runtime_error("not implemented");
+}
+
+uint32_t AdsProvidor_t::mapSymbolTipeToSize(const symbolDataType_t& symbolType ) {
+    switch (symbolType) {
+        case symbolDataType_t::e_bool:
+            return sizeof(bool);
+        case symbolDataType_t::e_char:
+            return sizeof(char);
+        case symbolDataType_t::e_double:
+            return sizeof(double);
+        case symbolDataType_t::e_float:
+            return sizeof(float);
+        case symbolDataType_t::e_int8_t:
+            return sizeof(int8_t);
+        case symbolDataType_t::e_int16_t:
+            return sizeof(int16_t);
+        case symbolDataType_t::e_int32_t:
+            return sizeof(int32_t);
+        case symbolDataType_t::e_int64_t:
+            return sizeof(int64_t);
+        case symbolDataType_t::e_string:
+            throw std::runtime_error("string is not supported");
+        case symbolDataType_t::e_uint8_t:
+            return sizeof(uint8_t);
+        case symbolDataType_t::e_uint16_t:
+            return sizeof(uint16_t);
+        case symbolDataType_t::e_uint32_t:
+            return sizeof(uint32_t);
+        case symbolDataType_t::e_uint64_t:
+            return sizeof(uint64_t);
+        default:
+            throw std::runtime_error("unknown symbol type");
+    }
+}
+
+uint32_t AdsProvidor_t::durationToNs(std::chrono::high_resolution_clock::duration duration) {
+    return static_cast<uint32_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count() / 100);
+}
+
+void AdsProvidor_t::subscribeSymbol(symbolDefinition_t& symbolDefinition) {
+    if (symbolDefinition.subscribed)
+        throw std::runtime_error("symbol already subscribed");
+    symbolDefinition.subscribed = true;
+
+    const AdsNotificationAttrib attrib = {
+        mapSymbolTipeToSize(symbolDefinition.symbolType),
+        ADSTRANS_SERVERONCHA,
+        durationToNs(symbolDefinition.expirationDuration),
+        { 4000000 }
+    };
+
+    //AdsNotification notification = {_device.value(), symbolDefinition.symbolName, attrib, &symbolChangedCallback, 0xBEEFDEAD};
+
+    //symbolDefinition.adsNotification.emplace();
+}
+
+void AdsProvidor_t::unsubscribeSymbols() {
+    throw std::runtime_error("not implemented");
+// delete ads notification attrib and AdsNotification
+}
--- src/ADSProvidor.h
+++ src/ADSProvidor.h
@@ -5,11 +5,121 @@
 export module AdsProvidor;
 
 import ProcessDataBuffer;
+import <string>;
+import <vector>;
+import <chrono>;
+import <concepts>;
+import <type_traits>;
+import <thread>;
+
+#include "AdsLib.h"
+#include "AdsDevice.h"
+#include "AdsVariable.h"
+#include "AdsNotificationOOI.h"
+
+export enum class symbolDataType_t {
+    e_uint8_t = 0,
+    e_uint16_t,
+    e_uint32_t,
+    e_uint64_t,
+    e_int8_t,
+    e_int16_t,
+    e_int32_t,
+    e_int64_t,
+    e_float,
+    e_double,
+    e_bool,
+    e_char,
+    e_string
+};
+
+struct symbolDefinition_t {
+    std::string symbolName;
+    symbolDataType_t symbolType;
+
+    // max time allowed to wait for value change (manual read required)
+    std::chrono::high_resolution_clock::duration expirationDuration;
+    std::chrono::high_resolution_clock::time_point lastRead;
+
+    // true, if value is subscribed to
+    bool subscribed = false;
+    std::optional<AdsNotification> adsNotification;
+};
+
+/*! Due to constraints to the callback function, subscribing is currently not implemented
+ * This may change if the performance is terrible, and significant effort on optimizations is required
+*/
 
 export class AdsProvidor_t {
 public:
-AdsProvidor_t(ProcessDataBuffer_t& processDataBuffer);
+    explicit AdsProvidor_t(ProcessDataBuffer_t& processDataBuffer, AmsNetId remoteAmsNetId, std::string remoteIPv4, AmsNetId localAmsNetId);
+    ~AdsProvidor_t();
+    void addSymbol(const std::string& symbolName, symbolDataType_t symbolType, std::chrono::high_resolution_clock::duration scrapingTime);
 
 private:
+
+    static void subscribeSymbols();
+    static void unsubscribeSymbols();
+
+    /*!
+     * @brief subscribes to symbol
+     * @param symbolDefinition symbol to subscribe to
+     */
+    void subscribeSymbol(symbolDefinition_t& symbolDefinition);
+
+    /*!
+     * @brief get from symboltype the size
+     * @param symbolType type of symbol
+     * @return size of symbol
+     */
+    static uint32_t mapSymbolTipeToSize(const symbolDataType_t& symbolType );
+
+    /*!
+     * @brief is called every time a symbol changes
+     * @param addr netId of remote
+     * @param notification actual callback data
+     * @param user user
+     */
+    void symbolChangedCallback(const AmsAddr* addr, const AdsNotificationHeader* notification, uint32_t user);
+
+    /*!
+     * @brief uses a symbol definition to set the symbol to a new value
+     */
+    void updateSymbolProcessDataBuffer(symbolDefinition_t& symbolDefinition, std::string value) const;
+    void updateSymbolProcessDataBuffer(symbolDefinition_t &symbolDefinition, const char value) const { updateSymbolProcessDataBuffer (symbolDefinition, std::string{value});}
+    void updateSymbolProcessDataBuffer(symbolDefinition_t& symbolDefinition, const bool value) const { updateSymbolProcessDataBuffer (symbolDefinition, (value ? std::string("true") : std::string("false"))); }
+    template <typename T>
+    void updateSymbolProcessDataBuffer(symbolDefinition_t& symbolDefinition, const T value) requires std::is_integral_v<T> || std::is_floating_point_v<T> { updateSymbolProcessDataBuffer (symbolDefinition, std::to_string(value)); }
+
+    /*!
+     * @brief this function checks time last read from symbols and reads them if necessary.
+     */
+    void forceReadSymbol();
+
+    void readSymbol(symbolDefinition_t& symbolDefinition);
+
+    /*!
+     * Converts a duration to a time that is n*100ns
+     * @param duration duration to convert
+     * @return *100ns
+     */
+    static uint32_t durationToNs(std::chrono::high_resolution_clock::duration duration);
+
+    /*!
+     * @brief this is the main thread function
+     */
+    void threadLoop();
+
+    //! initialize object after calling bhf::ads::SetLocalAddress in the constructor.
+    //! expect this variable to ALWAYS have a device.
+    std::optional<AdsDevice> _device;
+
+    //! this object should be created in the constructor and destroyed with the destructor
+    std::optional<std::jthread> _thread;
+
+
     ProcessDataBuffer_t& _processDataBuffer;
+
+    std::mutex _symbolNamesMutex;
+    std::vector<symbolDefinition_t> _symbolNames;
 };
